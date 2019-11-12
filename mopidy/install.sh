d11a596c3b07ed722bc4ac077f53620774c1e749
Phil Howard <phil@gadgetoid.com>
2019-11-12 10:53:48 +0000

    Drop sudo from install, add config.txt entries

--- mopidy/install.sh
+++ mopidy/install.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
 
+function add_to_config_text {
+    CONFIG_LINE = "$1"
+    CONFIG = "$2"
+    sed -i "s/^#$CONFIG_LINE/$CONFIG_LINE/" $CONFIG
+    if ! grep -q "$CONFIG_LINE1" $CONFIG; then
+		printf "$CONFIG_LINE\n" >> $CONFIG
+    fi
+}
+
+# Enable SPI
+raspi-config nonint do_spi 0
+
+# Add necessary lines to config.txt (if they don't exist)
+add_to_config_text "gpio=25=op,dh" /boot/config.txt
+add_to_config_text "dtoverlay=hifiberry-dac" /boot/config.txt
+
 # Install apt list for Mopidy, see: https://docs.mopidy.com/en/latest/installation/debian/.
-wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
-sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
+wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
 
 # Update apt
-sudo apt update
+apt update
 
 # Install dependencies
-sudo apt-get install python-rpi.gpio python-spidev python-pip python-imaging python-numpy
+apt-get install python-rpi.gpio python-spidev python-pip python-imaging python-numpy
 
 # install Mopidy and core plugins for Spotify and the Iris web UI
-sudo apt install mopidy mopidy-spotify mopidy-iris
+apt install mopidy mopidy-spotify mopidy-iris
 
 # Install support plugins for Pirate Audio
-sudo pip install Mopidy-PiDi pidi-display-pil pidi-display-st7789 mopidy-raspberry-gpio
+pip install Mopidy-PiDi pidi-display-pil pidi-display-st7789 mopidy-raspberry-gpio
 
 # Populate mopidy.conf with complete list of defaults
-mopidy config | sudo tee /etc/mopidy/mopidy.conf
+mopidy config > /etc/mopidy/mopidy.conf
 
 # Set IP for mpd and http to public
-sudo sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mopidy/mopidy.conf
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mopidy/mopidy.conf
 
 # Change to alsa audio sink
-sudo sed -i "s/autoaudiosink/alsasink/g" /etc/mopidy/mopidy.conf
+sed -i "s/autoaudiosink/alsasink/g" /etc/mopidy/mopidy.conf
 
 # Set mixer_volume to 40
-sudo sed -i "s/mixer_volume = $/mixer_volume = 40/g" /etc/mopidy/mopidy.conf
+sed -i "s/mixer_volume = $/mixer_volume = 40/g" /etc/mopidy/mopidy.conf
 
 # MAYBE?: Remove the sources.list to avoid any future issues with apt.mopidy.com failing
-# sudo rm -f /etc/apt/sources.list.d/mopidy.list
+# rm -f /etc/apt/sources.list.d/mopidy.list
