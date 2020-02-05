1a1822d96c26a0d5e688b53dad11bab3c5868a99
Philip Howard <phil@pimoroni.com>
2020-02-05 10:50:42 +0000

    Add new GPIO for volume up

--- mopidy/install.sh
+++ mopidy/install.sh
@@ -147,6 +147,7 @@ bcm5 = play_pause,active_low,250
 bcm6 = volume_down,active_low,250
 bcm16 = next,active_low,250
 bcm20 = volume_up,active_low,250
+bcm24 = volume_up,active_low,250
 
 [pidi]
 enabled = true
